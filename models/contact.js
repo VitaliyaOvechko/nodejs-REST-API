0081fef279ffd1bbfc1f37b45427866ae1cc0163
Vitaliya Ovechko <ovechko.vita@gmail.com>
2023-12-15 18:31:19 +0200

    add hw-3

---
+++ models/contact.js
@@ -0,0 +1,58 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+const { handleMongooseError } = require("../helpers");
+
+const contactSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+contactSchema.post("save", handleMongooseError);
+
+const addSchema = Joi.object({
+  name: Joi.string()
+    .messages({
+      "any.required": `missing required name field`,
+    })
+    .required(),
+  email: Joi.string()
+    .email()
+    .messages({
+      "any.required": `missing required email field`,
+    })
+    .required(),
+  phone: Joi.string()
+    .messages({
+      "any.required": `missing required phone field`,
+    })
+    .required(),
+  favorite: Joi.boolean(),
+});
+
+const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required(),
+});
+
+const schemas = {
+  addSchema,
+  updateFavoriteSchema,
+};
+const Contact = model("contact", contactSchema);
+
+module.exports = {
+  Contact,
+  schemas,
+};
