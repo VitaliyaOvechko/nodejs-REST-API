798bfd8a8f50226ca52d7cddeada0df37746e004
VitaliyaOvechko <113234981+VitaliyaOvechko@users.noreply.github.com>
2023-12-16 17:18:20 +0200

    hw 3_mongodb (#2)
    * add hw-3
    
    * add app

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -1,11 +1,13 @@
 const { HttpError } = require("../helpers");
+const { schemas } = require("../models/contact");
 
 const validateBody = (schema) => {
   const func = (req, res, next) => {
-    if (!req.body) {
+    if (!req.body || Object.keys(req.body).length === 0) {
+      if (schema === schemas.updateFavoriteSchema)
+        throw HttpError(400, "missing fields favorite");
       throw HttpError(400, "missing fields");
     }
-
     const { error } = schema.validate(req.body);
 
     if (error) {
