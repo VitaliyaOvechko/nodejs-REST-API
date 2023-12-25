3742645fe74f55cbad6d6bb9216c22ff1482e515
VitaliyaOvechko <113234981+VitaliyaOvechko@users.noreply.github.com>
2023-12-25 11:07:13 +0200

    add method for change avatar (#4)

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 
+//
+// const multer = require("multer");
+// const path = require("path");
+// const fs = require("fs/promises");
+// const { nanoid } = require("nanoid");
+//
+
 require("dotenv").config();
 
 const authRouter = require("./routes/api/auth");
@@ -14,6 +21,47 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
+
+// app.use(express.static("public"));
+// const tempDir = path.join(__dirname, "temp");
+
+// const multerConfig = multer.diskStorage({
+//   destination: tempDir,
+// });
+
+// //мідлвара
+// const upload = multer({
+//   storage: multerConfig,
+// });
+
+// const contacts = [];
+
+// const contactDir = path.join(__dirname, "public", "contact");
+
+// app.post("/api/contacts", upload.single("avatar"), async (req, res) => {
+//   console.log(req.body); //інофрмація про текстові поля запиту
+//   console.log(req.file); //інформація про файл
+
+//   const { path: tempUpload, originalname } = req.file;
+//   const resultUpload = path.join(contactDir, originalname);
+//   await fs.rename(tempUpload, resultUpload);
+
+//   const avatar = path.join("contacts", originalname);
+//   const newContact = {
+//     id: nanoid(),
+//     ...req.body,
+//     avatar,
+//   };
+
+//   contacts.push(newContact);
+
+//   res.status(201).json(newContact);
+// });
+// //якщо очікуємо кілька файлів - upload.array("avatar", кількість фалів)
+// //якщо очікуємо кілька файлів в різних полях-
+// // upload.fields([{name:"avatar", maxCount:1}, {name:"cover", maxCount:2}])
+// //
 
 app.use("/api/auth", authRouter);
 app.use("/api/contacts", contactsRouter);
